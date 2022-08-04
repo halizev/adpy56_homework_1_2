e5ab4c30d8ff736156dfbae5d0aa7fbb5de5ae00
halizev <a@halizev.ru>
2022-08-04 19:36:18 +0300

    second_commit

--- main.py
+++ main.py
@@ -1,30 +1,52 @@
 import csv
 import re
 
-with open("phonebook_raw.csv", encoding='windows-1251') as f:
-    rows = csv.reader(f, delimiter=",")
-    contacts_list = list(rows)
 
-# TODO 1: выполните пункты 1-3 ДЗ
-    # Преобразовать ФИО
-for contact in contacts_list:
-    i = 1
-    for contact_part in contact[1:2]:
-        if contact_part != '':
-            contact[0] += ' ' + contact_part
-            contact[i] = ''
-            i += 1
-    contact[0:2] = contact[0].split(' ')
-    print(contact)
-    # Преобразовать номер
-    # Уточнить добавочный
-    # Найти дубли
+def read_csv(file_name):
+    with open("phonebook_raw.csv", encoding='windows-1251') as f:
+        rows = csv.reader(f, delimiter=",")
+        contacts_list = list(rows)
+        return contacts_list
 
-pattern = re.compile("кекw")
+
+def format_fullname(contacts_list):
+    name_pattern = r'^([А-ЯЁа-яё]+)(\s*)(\,?)([А-ЯЁа-яё]+)(\s*)(\,?)([А-ЯЁа-яё]*)'
+    name_repl = r'\1,\4,\7'
+    contacts_list_replaced = list()
+    for contact in contacts_list:
+        contact_string = ','.join(contact)
+        contact_replaced = re.sub(name_pattern, name_repl, contact_string)
+        contact_list = contact_replaced.split(',')
+        contacts_list_replaced.append(contact_list)
+        print(contact)
+        print(contact_list)
+    return contacts_list_replaced
+
+
+def format_number(contacts_list):
+    number_pattern = r'(\+7|8)\s*\(*(\d{3})\)*\s*\-*(\d{3})\s*\-*(\d{2})\s*\-*(\d{2}\s*)\(*(доб*\.)*\s*(\d+)*\)*'
+    number_repl = r'+7(\2)\3-\4-\5\6\7'
+    contacts_list_replaced = list()
+    for contact in contacts_list:
+        contact_string = ','.join(contact)
+        contact_replaced = re.sub(number_pattern, number_repl, contact_string)
+        contact_list = contact_replaced.split(',')
+        contacts_list_replaced.append(contact_list)
+
+    return contacts_list_replaced
+
+
+def write_file(contacts_list):
+    with open("phonebook.csv", "w", newline='') as f:
+        datawriter = csv.writer(f, delimiter=',')
+        datawriter.writerows(contacts_list)
+
+
+contacts_list = read_csv('phonebook_raw.csv')
+contacts_list = format_fullname(contacts_list)
+contacts_list = format_number(contacts_list)
+
+write_file(contacts_list)
 
 # TODO 2: сохраните получившиеся данные в другой файл
 # код для записи файла в формате CSV
-with open("phonebook.csv", "w") as f:
-  datawriter = csv.writer(f, delimiter=',')
-  # Вместо contacts_list подставьте свой список
-  datawriter.writerows(contacts_list)
